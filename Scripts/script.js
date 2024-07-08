71f54c7f57d880f23d2d27baacead3c3d7f4d931
Angelo <148043527+bbrushh@users.noreply.github.com>
2024-07-08 17:45:36 +0200

    Modifiche CSS su lobby.html + adjustments
    - Provato ad inserire il tag script nell'head, dovrebbe funzionare allo stesso modo
    - inseriti alcuni ID e attributi su lobby, da completare
    - inserite modifiche CSS su elementi di lobby, integrate con commenti
    - PROBLEMA: con script.js che prende solo il primo comando

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,11 @@
-const startGame = document.getElementById("startGame");
+const startGame = document.querySelector("#startGame");
 startGame.addEventListener("click" ,() => {
     window.location.href = "lobby.html"
-})
+});
 
+const passRangeController = document.querySelector('#passRangeInput');
+const passRangeOutput = document.querySelector('#passRangeOutput');
+
+passRangeController.addEventListener('input', (e) => {
+  passRangeOutput.value = e.target.value;
+});
