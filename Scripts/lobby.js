d1fac3933285ae2017e0dc83c6708c0a7da53256
Angelo <148043527+bbrushh@users.noreply.github.com>
2024-07-09 15:50:29 +0200

    Piccoli adjustments grafici + funzione in Lobby.js
    - Migliorie grafiche sugli input
    - Tentativo di creare la funzione che crea degli input dei nomi giocatore (rossi e blu) seguendo l'input del numero giocatori per squadra: non funziona ancora. Al momento gli input di HTML sono messi a commento, quindi non si vedranno nella pagina. Per vederli basta rimuovere il commento.

--- Scripts/lobby.js
+++ Scripts/lobby.js
@@ -1,3 +1,5 @@
+//SLIDER PASSO
+
 const passRangeInput = document.querySelector('#passRangeInput');
 const passRangeOutput = document.querySelector('#passRangeOutput');
 
@@ -5,7 +7,9 @@ passRangeInput.addEventListener("input", (e) =>
     {
         passRangeOutput.value = e.target.value;
     }
-)
+);
+
+//SLIDER TURNI
 
 const turnRangeInput = document.querySelector('#turnRangeInput');
 const turnRangeOutput = document.querySelector('#turnRangeOutput');
@@ -13,4 +17,25 @@ turnRangeInput.addEventListener("input", (e) =>
     {
         turnRangeOutput.value = e.target.value;
     }
-)
+);
+
+//FUNZIONE CHE CREA INPUT DEI NOMI GIOCATORE SEGUENDO L'INPUT DEL NUMERO DI GIOCATORI - NON FUNZIONANTE
+
+const redPlayers = document.querySelector('#redPlayers');
+const bluePlayers = document.querySelector('#bluePlayers');
+const numPlayers = document.querySelector('#numPlayers');
+
+/*numPlayers.addEventListener("input", (e) => {
+    let i = 0;
+    while(i<=e.target.value){
+        this.redPlayersInput.createElement('input');
+        this.redPlayersInput.setAttribute('type', 'text');
+        this.redPlayersInput.setAttribute('placeholder', `Giocatore ${i} - Rosso`);
+        redPlayers.appendChild(document.createElement('input'));
+        this.bluePlayersInput = document.createElement('input');
+        this.bluePlayersInput.setAttribute('type', 'text');
+        this.bluePlayersInput.setAttribute('placeholder', `Giocatore ${i} - Blu`);
+        bluePlayers.appendChild(bluePlayersInput);
+        i++;
+    }
+})*/
