4e339c3e4e12c4ea293b9f856c01d708f94f9260
Angelo <148043527+bbrushh@users.noreply.github.com>
2024-07-16 09:36:11 +0200

    Fix grafico per le parole dentro la carta
    - Fixate le distanze e dimensioni delle parole nella carta
    - Introdotto (come commento) quello che probabilmente sarà lo script per gestire il cambio colore della carta con i nuovi bordi che avete inserito

--- Scripts/match.js
+++ Scripts/match.js
@@ -27,14 +27,18 @@ const redButton = document.getElementById("redButton");
 const blueButton = document.getElementById("blueButton");
 const body = document.querySelector("body");
 const cardImg = document.getElementById("cardImg");
+const wordContainer = document.getElementById("wordContainer");
 redButton.addEventListener("click", () => {
     body.style.background = 'rgb(240,91,102)';
     body.style.background = 'radial-gradient(circle, rgba(240,91,102,1) 0%, rgba(135,1,11,1) 68%)';
     cardImg.setAttribute('src', '../Assets/carta_rosso.PNG');
+   // wordContainer.setAttribute('background-image', 'url("./Assets/carta_bordo_interno_rosso.png")');
+    
 
 });
 blueButton.addEventListener("click", () => {
     body.style.background = 'rgb(0,166,229)';
     body.style.background = 'radial-gradient(circle, rgba(0,166,229,1) 0%, rgba(0,38,150,1) 68%)';
     cardImg.setAttribute('src', '../Assets/carta_blu.PNG')
+    // wordContainer.setAttribute('background-image', 'url("./Assets/carta_bordo_interno_blu.png")');
 });
