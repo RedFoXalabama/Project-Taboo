cea838281445a531ed3d82e5f6ae53c334bacd75
Gianfranco Baccarella <90854992+RedFoXalabama@users.noreply.github.com>
2024-08-10 12:55:12 +0200

    :arrow_up: node_modules

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const port = 5500;
-const http = require("http");
-const app = http.createServer((req, res) => {
-    console.log(http);
-    res.write("<h1>Prova di testo</h1>");
-    res.end();
-});
+const express = require("express");
+const port = 3000;
+const cors = require('cors');
+const mongoose = require("mongoose");
+const collectionRoutes = require("./routes/cards");
+const path = "mongodb+srv://gianfrancobaccarella:project-taboo-password@project-taboo-db.oajpkwv.mongodb.net/?retryWrites=true&w=majority&appName=Project-Taboo-DB";
 
-app.listen(port, () => {
-    console.log("Il server ti sente sulla porta: " + port);
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
+//app.use(express.static("static"));
+//app.use("/api/collections", collectionRoutes);
+
+mongoose.connect(path).then( () => {
+    console.log("Connessione al Database riuscita");
+    app.listen(port, () => {
+        console.log("Connesso sulla porta 3000");
+    })
+}).catch((err) => {
+    console.log(err)
+});
