16cf5627387970d8c18fb3aa91bb90467eba0fab
Gianfranco Baccarella <90854992+RedFoXalabama@users.noreply.github.com>
2024-08-18 11:44:55 +0200

    :shit: parziale

--- project-taboo/src/components/Rules.jsx
+++ project-taboo/src/components/Rules.jsx
@@ -9,11 +9,21 @@ function Rules(props) {
                 <ul className="RulesElement">
                     <li>{props.item.element1}</li>
                     <li>{props.item.element2}</li>
-                    
+
                 </ul>
             </Paper>
             <img></img>
         </div>
+    )
+}
+
+function elementList(props) {
+    return (
+        <ul>
+            {props.item.elementList.map((element, index) => {
+                return <li key={index}>{element}</li>
+            })}
+        </ul>
     )
 }
 
