aca25245ef5251f7a0495b9e995422543046e2f6
Angelo <148043527+bbrushh@users.noreply.github.com>
2024-08-29 10:45:00 +0200

    :lipstick: Piccole modifiche alle ombre + altro
    - Le ombre di alcuni box sono diverse
    -  Aggiunto paragrafo nel footer :)
    - Sistemata doppia regola sul footer

--- project-taboo/src/components/Footer.jsx
+++ project-taboo/src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
     return (
         <footer>
             <p>© 2024 Hasbro. Tutti i diritti riservati. Taboo è un marchio registrato di Hasbro. Questo sito non è affiliato, approvato o sponsorizzato da Hasbro. Tutti i nomi, i loghi e le immagini dei giochi da tavolo sono di proprietà dei rispettivi titolari.</p>
+            <p>Progetto realizzato da Gianfranco Baccarella, Angelo Calò e Manuele Lin.</p>
             <img id="hasbroLogo"  src={hasbroLogo}/>
         </footer>
     );
