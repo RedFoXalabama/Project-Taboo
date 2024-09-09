cb6f487f4d973d528659f8fa4ebbe47949a5deb6
Angelo <148043527+bbrushh@users.noreply.github.com>
2024-09-09 12:45:19 +0200

    :speech_balloon: Link ai profili GitHub
    - Aggiunto link ai nostri profili GitHub, con colorazione viola quando si hovera col mouse

--- project-taboo/src/components/Footer.jsx
+++ project-taboo/src/components/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() { //FOOTER DELLA PAGINA
     return (
         <footer>
             <p>© 2024 Hasbro. Tutti i diritti riservati. Taboo è un marchio registrato di Hasbro. Questo sito non è affiliato, approvato o sponsorizzato da Hasbro. Tutti i nomi, i loghi e le immagini dei giochi da tavolo sono di proprietà dei rispettivi titolari.</p>
-            <p>Progetto realizzato da Gianfranco Baccarella, Angelo Calò e Manuele Lin.</p>
+            <p>
+                Progetto realizzato da <a href="https://github.com/RedFoXalabama" target="_blank">Gianfranco Baccarella</a>, <a href="https://github.com/bbrushh" target="_blank">Angelo Calò</a> e <a href="https://github.com/YuMo594" target="_blank">Manuele Lin</a>.
+            </p>
             <img id="hasbroLogo"  src={hasbroLogo}/>
         </footer>
     );
